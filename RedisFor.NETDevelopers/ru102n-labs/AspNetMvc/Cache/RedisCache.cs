128c20f947b6f0afe22b3f0a6b91c78850aad980
Niraj Patel <nirajp82@yahoo.com>
2023-12-16 20:01:16 -0500

    Initial Checkin - Hands-On Caching in ASP.NET

---
+++ RedisFor.NETDevelopers/ru102n-labs/AspNetMvc/Cache/RedisCache.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvc.Cache
+{
+    /// <summary>
+    /// Represents a utility class for managing Redis connections and databases.
+    /// </summary>
+    public class RedisCache
+    {
+        /// <summary>
+        /// Gets the lazy-initialized ConnectionMultiplexer instance.
+        /// </summary>
+        private static readonly Lazy<ConnectionMultiplexer> _lazyMuxer;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="Redis"/> class.
+        /// </summary>
+        static RedisCache()
+        {
+            // Configuration options for connecting to the Redis server
+            var options = new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" }, // Replace with the actual Redis server endpoint
+                Password = "" // Replace with the actual password if required
+            };
+
+            // Lazily initialize the ConnectionMultiplexer using a lambda expression
+            _lazyMuxer = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(options));
+        }
+
+        /// <summary>
+        /// Gets the shared ConnectionMultiplexer instance.
+        /// </summary>
+        static ConnectionMultiplexer _muxer = _lazyMuxer.Value;
+
+        /// <summary>
+        /// Gets the default Redis database instance.
+        /// </summary>
+        public static IDatabase Database 
+        {
+            get 
+            {
+               return _muxer.GetDatabase();
+            }
+        }
+    }
+}
--- RedisFor.NETDevelopers/ru102n-labs/section_3/section3.3/Redis.cs
+++ RedisFor.NETDevelopers/ru102n-labs/section_3/section3.3/Redis.cs
@@ -6,22 +6,37 @@ using System.Web;
 
 namespace section3._3
 {
+    /// <summary>
+    /// Represents a utility class for managing Redis connections and databases.
+    /// </summary>
     public class Redis
     {
+        /// <summary>
+        /// Gets the lazy-initialized ConnectionMultiplexer instance.
+        /// </summary>
         private static readonly Lazy<ConnectionMultiplexer> LazyMuxer;
 
         static Redis()
         {
+            // Configuration options for connecting to the Redis server
             var options = new ConfigurationOptions
             {
-                EndPoints = { "localhost:6379" },
-                Password = ""
+                EndPoints = { "localhost:6379" }, // Replace with the actual Redis server endpoint
+                Password = "" // Replace with the actual password if required
             };
 
+            // Lazily initialize the ConnectionMultiplexer using a lambda expression
             LazyMuxer = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(options));
         }
 
+        /// <summary>
+        /// Gets the shared ConnectionMultiplexer instance.
+        /// </summary>
         public static ConnectionMultiplexer Muxer => LazyMuxer.Value;
+
+        /// <summary>
+        /// Gets the default Redis database instance.
+        /// </summary>
         public static IDatabase Database => Muxer.GetDatabase();
     }
-}
+ }
